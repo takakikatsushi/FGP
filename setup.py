0da7831de0cf950c0b609816cffb1c2b831ff8ea
takakikatsushi <takakikastushi@outlook.com>
2022-10-28 02:38:49 +0900

    rename

--- setup.py
+++ setup.py
@@ -6,12 +6,12 @@ def _requirement_packages(filename):
 	return open(filename).read().splitlines()
 
 setup(
-	name="fgpnls",
+	name="figp",
 	version="0.1.0",
 	license="Creative Commons Attribution 4.0 International License",
-	description="Symbolic regression with FGP-NLS",
+	description="Symbolic regression with FIGP",
 	author="Katsushi Takaki",
-	url="https://github.com/takakikatsushi/FGP-NLS",
+	url="https://github.com/takakikatsushi/FGP",
 	packages=find_packages("src"), # detect python packages in the codes
 	package_dir={"":"src"},
 	#py_modules=[splittext(basename(path))[0] for path in glob('src/fgpnls/*.py')]
